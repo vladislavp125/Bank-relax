bd401194278938ae63bad628e7993da060b5e62f
LOKI LOKI <vladislavp2011@gmail.com>
2024-02-17 01:32:14 +0300

    final project

--- main.py
+++ main.py
@@ -36,38 +36,46 @@ def pass_try(user_pass, password_now):
 def create_user():
     year = 2024
     balance = 0
+    name = input('Введите ФИО: ')
     try:
-        name = input('Введите ФИО: ')
-        age = int(input('Введите год рождения: '))
+        age = int(input('Внимание: Год рождения должен состоять только из цифр!' + '\nВведите год рождения: '))
         login = str(input('Придумайте LOGIN: '))
         password = str(input('Придумайте пароль: '))
         print(f'Создан аккаунт: {name}  ({str(year - age)} лет)')
-        print('Аккаунт успешно зарегистрирован!')
-        login, password = hash_pass(login, password)
-        with open(f'{login}_data_user.txt', 'w', encoding='UTF-8') as file:
-            file.write('name ' + str(name) + '\n' + 'age ' + str(age) + '\n')
-        with open(f'{login}_balance.txt', 'w', encoding='UTF-8') as file:
-            file.write('balance' + str(balance))
-        return login, password, balance
     except ValueError:
-        print()
-        print('Возраст должен состоять только из цифр !!')
-        print()
+        age = int(input('Год рождения должен состоять из цифр, введите число:'))
+        login = str(input('Придумайте LOGIN: '))
+        password = str(input('Придумайте пароль: '))
+        print(f'Создан аккаунт: {name}  ({str(year - age)} лет)')
+    print('Аккаунт успешно зарегистрирован!')
+    login, password = hash_pass(login, password)
+    with open(f'{login}_data_user.txt', 'w', encoding='UTF-8') as file:
+        file.write('name.' + str(name) + '\n' + 'age.' + str(age) + '\n')
+    with open(f'{login}_balance.txt', 'w', encoding='UTF-8') as file:
+        file.write('balance ' + str(balance))
+    with open(f'{login}_transaction.txt', 'w', encoding='UTF-8') as file_data_transaction:
+        file_data_transaction.write('')
+    with open(f'{login}_limit_data.txt', 'w', encoding='UTF-8') as file:
+        file.write('limite 0')
+    with open(f'{login}_future_pay.txt', 'w', encoding='UTF-8') as file:
+        file.write('')
+    return login, password, balance
 
 
 def money_up(balance, login_to_up):
     try:
         cash_up = int(input('Введите сумму пополнения: '))
-        print('Счет успешно пополнен!')
         balance += cash_up
+        print('Счет успешно пополнен!')
         with open(f'{login_to_up}_balance.txt', 'w', encoding='UTF-8') as file_user_balance:
             file_user_balance.write('balance ' + str(balance))
         return balance
 
     except ValueError:
         print()
-        print('Чтобы пополнить баланс введите верную сумму, возможно вы пытаетесь ввести слово!')
+        print('Возможно вы пытаетесь ввести буквы!')
         print()
+        return balance
 
 
 def money_to_cash(balance, password, login_to_cash):
@@ -82,6 +90,7 @@ def money_to_cash(balance, password, login_to_cash):
             else:
                 balance -= cash_down
                 print('Снятие успешно завершено, ваш баланс: ' + str(balance))
+                return balance
         else:
             print('Неверный пароль! Попробуйте ещё раз.')
 
@@ -113,7 +122,6 @@ def transaction(transactions, login_tran):
         transactions[comment] = expected_amount
         count_tran = len(transactions)
         print('Ожидается ' + str(count_tran) + ' операций(я)')
-        string_for_copy = ''
         with open(f'{login_tran}_transaction.txt', 'a', encoding='UTF=8') as file_transaction:
             file_transaction.write(str(comment) + ' ' + str(expected_amount) + '\n')
     except ValueError:
@@ -140,12 +148,12 @@ def ran_transactions(transactions, limite, login_tran):
         for comment, expected_amount in list(transactions.items()):
             if int(expected_amount) < limite:
                 summ += int(expected_amount)
-                print('Транзакция "' + comment + '" на сумму ' + str(expected_amount) + 'руб. успешно применена.')
+                print(f'Транзакция {comment} на сумму {expected_amount} руб. успешно применена.')
                 del transactions[comment]
             else:
-                print('Транзакция "' + comment + '" на сумму ' + str(expected_amount) + 'руб. не может быть применена('
-                                                                                        'превышен лимит).')
-
+                print(
+                    f'Транзакция {comment} на сумму {expected_amount} руб. Не может быть применена(превышен лимит или '
+                    f'не установлен).')
         with open(f'{login_tran}_transaction.txt', 'w', encoding='UTF=8') as file_transaction:
             for comment, expected_amount in list(transactions.items()):
                 file_transaction.write(str(comment) + ' ' + str(expected_amount) + '\n')
@@ -183,36 +191,41 @@ def filter_transaction(transactions):
     threshold = int(input('Введите число от которого будем проверять сумму транзакций: '))
     for transactions in filter_transaction_sub(transactions):
         if int(transactions[1]) >= threshold:
-            print(f'Транзакция больше {threshold} рублей.: {transactions[0]}  {transactions[1]}')
+            print(f'Транзакция больше {threshold} рублей.: {transactions[0]}  {transactions[1]} р.')
 
 
 def future_pay(user_password, balance_user):
     login = str(input('Введите логин : '))
-    password = (input('Введите пароль : '))
-    if pass_try(password, user_password):
-        login_to_pay = str(input('Введите логин на который хотите отправить деньги: '))
-        summ = int(input('Введите сумму пополнения'))
-        if summ <= balance_user:
-            with open(f'{login_to_pay}_balance.txt', encoding='UTF-8') as file:
-                for line in file:
-                    name, balance = line.split()
-                    balance = int(balance)
-                    balance += summ
-                    balance_user -= balance
-                    balance = str(balance)
-            with open(f'{login_to_pay}_balance.txt', 'w', encoding='UTF-8') as file:
-                file.write(f'balance {balance}')
-            with open(f'{login}_balance.txt', 'w', encoding='UTF-8') as file:
-                file.write(f'balance {balance_user}')
-            print(f'Отложенный платеж успешно выполнен на сумму {summ}, отправлен на логи: {login_to_pay}')
-            print(f'На вашем счету осталось {balance_user}')
+    if login == user_data['login']:
+        password = (input('Введите пароль : '))
+        if pass_try(password, user_password):
+            login_to_pay = str(input('Введите логин на который хотите отправить деньги: '))
+            summ = int(input('Введите сумму пополнения: '))
+            if summ <= balance_user:
+                with open(f'{login_to_pay}_balance.txt', encoding='UTF-8') as file:
+                    for line in file:
+                        name, balance = line.split()
+                        balance = int(balance)
+                        balance += summ
+                        balance_user -= summ
+                        balance = str(balance)
+                with open(f'{login_to_pay}_balance.txt', 'w', encoding='UTF-8') as file:
+                    file.write(f'balance {balance}')
+                with open(f'{login}_balance.txt', 'w', encoding='UTF-8') as file:
+                    file.write(f'balance {balance_user}')
+                print(f'Отложенный платеж успешно выполнен на сумму {summ}, отправлен на логин: {login_to_pay}')
+                print(f'На вашем счету осталось {balance_user}')
+                return balance_user
+            else:
+                with open(f'{login}_future_pay.txt', 'a', encoding='UTF-8') as file:
+                    file.write(f'{login_to_pay} {summ}\n')
+                print(f'Не удалось пополнить указанный логин! операция будет выполнина, как только на балансе будет'
+                      f' достаточно средств')
+                return balance_user
         else:
-            with open(f'{login}_future_pay.txt', 'a', encoding='UTF-8') as file:
-                file.write(f'{login_to_pay} {summ}\n')
-            print(f'Не удалось пополнить указанный логин! операция будет выполнина, как только на балансе будет '
-                  f'достаточно средств')
+            print('Неверный пароль! ')
     else:
-        print('Неверный пароль! ')
+        print('Неверный логин')
 
 
 def future_pay_act(user_balance, user_login):
@@ -225,13 +238,13 @@ def future_pay_act(user_balance, user_login):
                         with open(f'{login_to_pay}_balance.txt', encoding='UTF-8') as file1:
                             for line1 in file1:
                                 name, balance = line1.split()
-                                balance = int(balance)
-                                balance += int(summ)
-                                user_balance -= int(summ)
-                                balance = str(balance)
+                            balance = int(balance)
+                            balance += int(summ)
+                            user_balance -= int(summ)
+                            balance = str(balance)
                             with open(f'{login_to_pay}_balance.txt', 'w', encoding='UTF-8') as file:
                                 file.write(f'balance {balance}')
-                            with open(f'{login}_balance.txt', 'w', encoding='UTF-8') as file:
+                            with open(f'{user_login}_balance.txt', 'w', encoding='UTF-8') as file:
                                 file.write(f'balance {user_balance}')
                             print(f'Отложенный платеж выполнен на сумму {summ}, отправлен на логин: {login_to_pay}')
                             print(f'На вашем счету осталось {user_balance}')
@@ -239,34 +252,34 @@ def future_pay_act(user_balance, user_login):
                                 file.write('')
                             return user_balance
                 else:
-                    break
+                    print('Недостаточно средств для перевода! ')
+                    return user_balance
     except:
-        print('непредвиденная ошибка')
+        print()
 
 
 def recover_data_YES(user_login, transactions, user_data_func):
-    try:
-        with open(f'{user_login}_transaction.txt', encoding='UTF-8') as file_data_transaction:
-            for line in file_data_transaction:
-                if line == '\n':
-                    continue
-                else:
-                    comment, expected_amount = line.split()
-                    transactions[comment] = expected_amount
-    except:
-        print("Файл с транзакциями отсутствует или поврежден")
+    with open(f'{user_login}_transaction.txt', encoding='UTF-8') as file_data_transaction:
+        for line in file_data_transaction:
+            if line == '\n':
+                continue
+            else:
+                comment, expected_amount = line.split()
+                transactions[comment] = expected_amount
 
     with open(f'{user_login}_data_user.txt', encoding='UTF-8') as file_user_data:
         for line in file_user_data:
             if line == '\n':
                 continue
             else:
-                key, valume = line.split()
+                key, valume = line.split('.')
                 user_data_func[key] = valume
+
     with open(f'{user_login}_balance.txt', encoding='UTF-8') as file_user_balance:
         for line in file_user_balance:
             key, valume = line.split()
             user_data_func[key] = valume
+
     with open(f'{user_login}_limit_data.txt', encoding='UTF-8') as file:
         for line in file:
             if line != '':
@@ -274,6 +287,7 @@ def recover_data_YES(user_login, transactions, user_data_func):
                 user_data_func[key] = valume
             else:
                 continue
+
     with open(f'login.passwordhash.txt', encoding='UTF-8') as file_hash:
         for line in file_hash:
             if user_login in line:
@@ -283,28 +297,32 @@ def recover_data_YES(user_login, transactions, user_data_func):
                 continue
 
     print(f'С возвращением {user_data_func['name']} !')
-    print(f'Ваш баланс: {user_data_func['balance']}')
+    print(f'Ваш баланс: {user_data_func['balance']} р.')
     return user_data_func
 
 
-def recover_login(user_login, transactions, user_data_func):
-    try:
+def recover_login(transactions, user_data_func):
+    v = {}
+    while True:
+        login = input('Введите логин: ')
         password = input('Введите пароль: ')
-        with open(f'login.passwordhash.txt', encoding='UTF-8') as file_hash:
-            for line in file_hash:
-                if user_login in line:
-                    user_login, password_old = line.split()
-                    if pass_try(password, password_old):
-                        user_data = recover_data_YES(user_login, transactions, user_data_func)
+        try:
+            with open(f'login.passwordhash.txt', encoding='UTF-8') as file_hash:
+                for line in file_hash:
+                    key, valume = line.split()
+                    v[key] = valume
+                if login in v:
+                    if pass_try(password, v[login]):
+                        user_data = recover_data_YES(login, transactions, user_data_func)
+                        user_data['login'] = login
                         return user_data
-                    else:
-                        print()
-                        print('Неверный пароль или логин!')
-                        print()
                 else:
-                    continue
-    except:
-        print("Аккаунт не найден")
+                    print()
+                    print('Неверный пароль или логин!')
+                    print()
+        except:
+            print("Аккаунт с таким логином не найден")
+            exit()
 
 
 def recover_sign_in():
@@ -330,58 +348,65 @@ def operation_user():
 
 
 if __name__ == "__main__":
-    Bank_login = input('Добро пожаловать, вы хотите зарегистрироваться или войти в аккаунт?(Войти/Рег): ')
-    if Bank_login == 'Войти':
-        login = input('Введите логин: ')
-        user_data = recover_login(login, transactions_data, user_data)
-        password = user_data[login]
-        balance = int(user_data['balance'])
-        limite = int(user_data['limite'])
-    else:
-        login, password, balance = recover_sign_in()
+    try:
+        Bank_login = input('Добро пожаловать, вы хотите зарегистрироваться или войти в аккаунт?(Войти/Рег): ')
+        if Bank_login == 'Войти':
+            user_data = recover_login(transactions_data, user_data)
+            login = user_data['login']
+            password = user_data[str(login)]
+            balance = int(user_data['balance'])
+            limite = int(user_data['limite'])
+        else:
+            login, password, balance = recover_sign_in()
+            user_data['login'] = login
+    except ValueError:
+        print('Некорректный ввод!!')
 
     while True:
-        future_pay_act(balance, login)
-        operation = operation_user()
-        if operation == 1:
-            balance = money_up(balance, login)
-            print()
+        try:
+            future_pay_act(balance, login)
+            operation = operation_user()
+            if operation == 1:
+                balance = money_up(balance, login)
+                print()
 
-        elif operation == 2:
-            balance = money_to_cash(balance, password, login)
-            print()
+            elif operation == 2:
+                balance = money_to_cash(balance, password, login)
+                print()
 
-        elif operation == 3:
-            check_balance(balance, password)
-            print()
+            elif operation == 3:
+                check_balance(balance, password)
+                print()
 
-        elif operation == 4:
-            transaction(transactions_data, login)
-            print()
+            elif operation == 4:
+                transaction(transactions_data, login)
+                print()
 
-        elif operation == 5:
-            limite = max_limite(login)
-            print()
+            elif operation == 5:
+                limite = max_limite(login)
+                print()
 
-        elif operation == 6:
-            balance += ran_transactions(transactions_data, limite, login)
-            with open(f'{login}_balance.txt', 'w', encoding='UTF=8') as file_user_balance:
-                file_user_balance.write('balance ' + str(balance))
-            print()
+            elif operation == 6:
+                balance += ran_transactions(transactions_data, limite, login)
+                with open(f'{login}_balance.txt', 'w', encoding='UTF=8') as file_user_balance:
+                    file_user_balance.write('balance ' + str(balance))
+                print()
 
-        elif operation == 7:
-            stat_expected_amount(transactions_data)
-            print()
+            elif operation == 7:
+                stat_expected_amount(transactions_data)
+                print()
 
-        elif operation == 8:
-            filter_transaction(transactions_data)
-            print()
+            elif operation == 8:
+                filter_transaction(transactions_data)
+                print()
 
-        elif operation == 9:
-            future_pay(password, balance)
+            elif operation == 9:
+                balance = future_pay(password, balance)
 
-        elif operation == 10:
-            print('Спасибо за пользование нашей программой, до свидания!')
-            break
-        else:
-            print('Некорректный номер операции, попробуйте снова :(')
+            elif operation == 10:
+                print('Спасибо за пользование нашей программой, до свидания!')
+                break
+            else:
+                print('Некорректный номер операции, попробуйте снова :(')
+        except ValueError:
+            print('Некорректный ввод!')
